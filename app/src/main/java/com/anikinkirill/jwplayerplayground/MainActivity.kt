58b7a6e0d827d222031ff16c76f804a2dac3fce7
ANIKINKIRILL <ky.anikin@mail.ru>
2020-09-15 14:27:11 +0300

    Add VPAID ad

--- app/src/main/java/com/anikinkirill/jwplayerplayground/MainActivity.kt
+++ app/src/main/java/com/anikinkirill/jwplayerplayground/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.longtailvideo.jwplayer.JWPlayerView
 import com.longtailvideo.jwplayer.configuration.PlayerConfig
-
+import com.longtailvideo.jwplayer.media.ads.AdBreak
+import com.longtailvideo.jwplayer.media.ads.AdSource
+import com.longtailvideo.jwplayer.media.ads.Advertising
 import com.longtailvideo.jwplayer.media.playlists.PlaylistItem
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +20,28 @@ class MainActivity : AppCompatActivity() {
 
         jwPlayerView = findViewById(R.id.jwplayer)
 
+        val adSchedule: MutableList<AdBreak> = ArrayList()
+
+        val adBreak = AdBreak.Builder()
+            .tag("https://stats.seedr.com/vast?gid=5f19631355f5cb79633d2c5b&debug=true")
+            .offset("pre")
+            .build()
+
+        adSchedule.add(adBreak)
+
+        val advertising = Advertising(AdSource.VAST, adSchedule)
+
         val playlistItem = PlaylistItem.Builder()
                 .file("https://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8")
                 .build()
 
         val playlist: MutableList<PlaylistItem> = ArrayList()
         playlist.add(playlistItem)
-        val config = PlayerConfig.Builder().playlist(playlist).build()
+        val config = PlayerConfig.Builder()
+            .playlist(playlist)
+            .advertising(advertising)
+            .autostart(true)
+            .build()
         jwPlayerView.setup(config)
     }
 
